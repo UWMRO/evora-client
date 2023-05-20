c84d382553374ce375cda00502e4e7c42be6c2c0
José Sánchez-Gallego <gallegoj@uw.edu>
2023-05-20 15:09:30 -0700

    Add shutdown spinner

--- src/components/OnOffFunctionality.jsx
+++ src/components/OnOffFunctionality.jsx
@@ -1,6 +1,10 @@
+import { useState } from "react";
 import { initialize, shutdown } from "../apiClient"
+import BeatLoader from 'react-spinners/BeatLoader';
 
 function OnOff({initialized, setInitialized}) {
+
+    const [shuttingDown, setShuttingDown] = useState(false);
 
     async function onInitialize() {
         console.log("Initializing Andor...")
@@ -8,12 +12,15 @@ function OnOff({initialized, setInitialized}) {
         console.log(msg)
         setInitialized(true)
     }
-    
+
     async function onShutdown() {
         console.log("Shutting down Andor...")
+        setShuttingDown(true)
+
         const msg = await shutdown()
         console.log(msg)
         setInitialized(false)
+        setShuttingDown(false)
         // console.log("Pinging Filter Wheel Connection")
         // const msg = await getFilterWheelStatus()
         // console.log(msg.message)
@@ -25,8 +32,15 @@ function OnOff({initialized, setInitialized}) {
                 Initialize
             </button>
             <button disabled={!initialized} onClick={onShutdown}>
-                Shut Down
+                { shuttingDown ? "Shutting Down ..." : "Shut Down" }
             </button>
+            <BeatLoader
+              cssOverride={{ verticalAlign: 'middle', alignContent: 'end' }}
+              color="red"
+              size={12}
+              loading={shuttingDown}
+              speedMultiplier={0.7}
+            />
         </fieldset>
     )
 }
